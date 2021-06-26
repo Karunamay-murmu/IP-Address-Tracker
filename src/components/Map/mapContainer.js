1db1a556d0e66afb4df0fe1f6433b224951ed3a8
Karunamay Murmu <Karunamaymurmu.km@gmail.com>
2021-06-26 11:25:25 +0530

    add marker to map

--- src/components/Map/mapContainer.js
+++ src/components/Map/mapContainer.js
@@ -1,17 +1,19 @@
 import React, { useState, useRef, useEffect } from 'react'
 import mapboxgl from "mapbox-gl";
 
+import iconLocation from "../../static/assets/icon-location.svg";
 import "../../static/stylesheets/css/map.css";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 function MapContainer() {
     const [lng, setLng] = useState(13.4049)
-    const [lat, setLat] = useState(52.5200)
+    const [lat, setLat] = useState(59.5200)
     const [zoom, setZoom] = useState(15)
 
     const mapContainer = useRef(null);
     const map = useRef(null);
+    const marker = useRef(null);
 
     useEffect(() => {
         if (map.current) return;
@@ -32,10 +34,18 @@ function MapContainer() {
         });
     });
 
+    useEffect(() => {
+        marker.current = new mapboxgl.Marker(marker.current).setLngLat([lng, lat]).addTo(map.current);
+    }, [])
+
+
     return (
         <div class="map-container">
             <div className="topbar">
                 Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={marker} className="marker">
+                <img src={iconLocation} alt="marker" />
             </div>
             <div ref={mapContainer} className="map" />
         </div>
